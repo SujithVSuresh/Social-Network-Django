340d25396c799e47fac6ec0a844f02371448480d
Sujith V S <sujitheternal13@gmail.com>
2021-08-17 11:16:31 +0530

    first commit

---
+++ social/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.db.models import fields
+from . models import Post, Comment
+
+
+class PostForm(forms.ModelForm):
+    body = forms.CharField(label='', widget=forms.Textarea(attrs={'rows':'3', 'placeholder':'Say Something...'}))
+
+    class Meta:
+        model = Post
+        fields = ['body']
+
+class CommentForm(forms.ModelForm):
+    comment = forms.CharField(label='', widget=forms.Textarea(attrs={'rows':'3', 'placeholder':'Add Comments'}))
+
+    class Meta:
+        model = Comment
+        fields = ['comment']        
